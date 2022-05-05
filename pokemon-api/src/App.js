f8ee95711c88d074203760fdadb4dd37e7e987f2
amanda.batista <amanda.batista@thoughtworks.com>
2022-05-05 14:32:52 -0300

    Initial commit - React project created

---
+++ pokemon-api/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div >
+      
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+    </div>
+  );
+}
+
+export default App;
