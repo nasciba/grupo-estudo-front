8f10deb22fdd3baa35d4a4081aacdbb1a80a51a7
amanda.batista <amanda.batista@thoughtworks.com>
2022-05-24 15:03:26 -0300

    Implemented grid display on App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
+import Content from "./components/Content/Content"
+import "./app.css";
 
 function App() {
   return (
-    <div >
+    <div className="app" >
       <Header/>
+      <Content/>
        <Footer/>
     </div>
   );
